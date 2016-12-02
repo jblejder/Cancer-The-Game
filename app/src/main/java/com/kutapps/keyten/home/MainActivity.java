725e24dbab770bdb3b0779e09303476e646b4934
Mateusz Kwieciński <mateusz.kwiecinski@angrynerds.pl>
2016-12-02 11:31:11 +0100

    Added retrolambda. Added binding adapters

--- app/src/main/java/com/kutapps/keyten/home/MainActivity.java
+++ app/src/main/java/com/kutapps/keyten/home/MainActivity.java
@@ -3,29 +3,25 @@ package com.kutapps.keyten.home;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.kutapps.keyten.R;
 import com.kutapps.keyten.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     HomeViewModel       viewModel;
     ActivityMainBinding binding;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         viewModel = new HomeViewModel();
         binding.setModel(viewModel);
 
-        findViewById(R.id.send).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewModel.setKeyten();
-            }
-        });
+        binding.send.setOnClickListener(v -> viewModel.setKeyten());
     }
 }
