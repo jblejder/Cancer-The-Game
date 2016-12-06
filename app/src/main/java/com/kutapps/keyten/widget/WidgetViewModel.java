10d0c56df5284daa596477ab80fd324fe384fad3
Jakub Blejder <jakub.blejder@angrynerds.pl>
2016-12-06 14:27:31 +0100

    app widget

---
+++ app/src/main/java/com/kutapps/keyten/widget/WidgetViewModel.java
@@ -0,0 +1,8 @@
+package com.kutapps.keyten.widget;
+
+import android.databinding.ObservableBoolean;
+
+public class WidgetViewModel
+{
+    public ObservableBoolean keyten = new ObservableBoolean(false);
+}
